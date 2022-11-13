06e43c36b9a7faba93c0d8333fe9c62707baf392
chenxitianyao <34850260+chenxitianyao@users.noreply.github.com>
2022-11-13 08:26:59 +0000

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -102,7 +102,26 @@ const USER_CONFIG = {
     },
   ],
 
-  
+   {
+
+     name: '偷懒的小蜜蜂',
+
+     id: 'osZhI67GIj0AsmiruHSP_-MTjJOk'
+      useTemplateId: 'K8FvnxRDqTlFZtOhQzhioaaiVIAKaXdmrfT_Yp6VVCE',
+
+    province: '河北',   
+
+      city: '石家庄',
+
+     horoscopeDate: '9-17',
+
+     horoscopeDateType: '今日',
+
+     openUrl: 'https://mp.weixin.qq.com/s?__biz=MzkwMjQwMzY0Mg==&mid=2247483682&idx=1&sn=002e49362ea8432224dd567248eb3045&chksm=c0a74277f7d0cb6192b354626eb1b374c443e112d50cf269ad437c675611e2ef3ea5e107dcd6&token=1205761133&lang=zh_CN#rd',
+      festivals: [  {"type": "*生日", "name": "father", "year": "1956", "date": "08-13"},],
+      customizedDateList: [  {"type": "*生日", "name": "自己", "year": "1992", "date": "7-20"},],
+      courseSchedule: null
+    },
 
   {
   name:'种花家的小户同学',
